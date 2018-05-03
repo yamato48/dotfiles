2eb97820afedb7c6b4899214c9dfb84cf2ae355d
yamato48 <yamd1048@gmail.com>
2018-05-03 17:26:55 +0900

    add link .bashrc
    .bashrcのシンボリックリンク

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,5 +2,5 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
-
